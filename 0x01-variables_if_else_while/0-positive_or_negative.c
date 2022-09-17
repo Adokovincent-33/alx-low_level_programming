c075a9868d358ace4b4dc662a96e540d731aa9ed
Your Name <you@example.com>
2022-09-17 07:41:24 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  *  * main - entry point
  *   *
  *    * Return:always return 0
- *    */
+ */
 int main(void)
 {
 		int n;
@@ -17,11 +17,9 @@ int main(void)
 							{
 										printf("%d is negative\n", n);
 											} else if (n == 0)
-													{
-																printf("%d is zero\n", n);
+													{																printf("%d is zero\n", n);
 																	} else
-																			{
-																						printf("%d is positive\n", n);
+																			{																						printf("%d is positive\n", n);
 																							}
 																		return (0);
 }
