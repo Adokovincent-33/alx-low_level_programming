257a65ed11be9398fa161540a6f100e07db64d37
Your Name <you@example.com>
2022-09-17 03:29:38 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  * *
  * *Return: Always 0
  * */
-int main (void)
+int main(void)
 {
-	printf(" with proper grammar, but the outcome is a piece of art, \n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
+
