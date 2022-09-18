b9ec6d092171a97a7a6eeecc938d551014764265
Your Name <you@example.com>
2022-09-18 07:03:51 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 	 *  *
 	 *  * Return: always 0
 	 */
-int main(void)
+int main(void);
 {
 		printf("Size of char: %ld byte(s)\n", sizeof(char));
 			printf("Size of an int: %ld byte(s)\n", sizeof(int));
@@ -12,7 +12,7 @@ int main(void)
 					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 					printf("Size of a float: %ld byte(s)\n", sizeof(float));
 			return (0);
-		}
+}
 
 
 
